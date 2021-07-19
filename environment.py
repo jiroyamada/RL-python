d113d5e2d642698d1f3f4f4036f21ec0ca376627
jiroyamada <poirot343@outlook.jp>
2021-07-19 16:20:14 +0900

    code1-1

--- environment.py
+++ environment.py
@@ -1,3 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
+class State():
+
+    def __init__(self, row=-1, column=-1):
+        self.row = row
+        self.column = column
+
+    def __repr__(self):
+        return "<State: [{}, {}]>".format(self.row, self.column)
+
+    def clone(self):
+        return State(self.row, self.column)
+
+    def __hash__(self):
+        return hash((self.row, self.column))
+
+    def __eq__(self, other):
+        return self.row == other.row and self.column == other.column
+
