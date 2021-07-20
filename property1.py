91f3e0b6915ee46fdb5ea80fa98f65deae212d78
jiroyamada <poirot343@outlook.jp>
2021-07-20 18:13:35 +0900

    プロパティ練習

--- property1.py
+++ property1.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+"""
 class Cat:
     def __init__(self):
         self.name = "ミケ"
@@ -13,3 +13,30 @@ print(cat.name)
 cat1 = Cat()
 print(cat1.name)
 print(cat1.get_name())
+"""
+
+class PropertyClass:
+      def __init__(self, msg):
+        self.message = msg
+
+      def __str__(self):
+        return self.message
+"""
+      @property
+      def message(self):
+        return self.__message
+
+      @message.setter
+      def message(self, value):
+        if value != '':
+          self.__message = value
+"""
+
+pc= PropertyClass('Hello')
+pc1 = PropertyClass('Hello1')
+
+print(pc.message)
+print(pc1.message)
+
+pc.message = 'Hello1'
+print(pc.message)
