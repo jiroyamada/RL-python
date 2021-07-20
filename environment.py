4666dff4e1f5f73547d304137925be9a2f0658d2
jiroyamada <poirot343@outlook.jp>
2021-07-21 01:15:09 +0900

    報酬関数

--- environment.py
+++ environment.py
@@ -71,3 +71,80 @@ class Environment():
                 if self.grid[row][column] != 9:
                     states.append(State(row, column))
         return states
+
+
+    def transit_func(self, state, action):
+        transition_probs = {}
+        if not self.can_action_at(state):
+            # Already on the terminal cell.
+            return transition_probs
+
+        opposite_direction = Action(action.value * -1)
+
+        for a in self.actions:
+            prob = 0
+            if a == action:
+                prob = self.move_prob
+            elif a != opposite_direction:
+                prob = (1 - self.move_prob) / 2
+
+            next_state = self._move(state, a)
+            if next_state not in transition_probs:
+                transition_probs[next_state] = prob
+            else:
+                transition_probs[next_state] += prob
+
+        return transition_probs
+
+    def can_action_at(self, state):
+        if self.grid[state.row][state.column] == 0:
+            return True
+        else:
+            return False
+
+    def _move(self, state, action):
+        if not self.can_action_at(state):
+            raise Exception("Can't move from here!")
+
+        next_state = state.clone()
+
+        # Execute an action (move).
+        if action == Action.UP:
+            next_state.row -= 1
+        elif action == Action.DOWN:
+            next_state.row += 1
+        elif action == Action.LEFT:
+            next_state.column -= 1
+        elif action == Action.RIGHT:
+            next_state.column += 1
+
+        # Check whether a state is out of the grid.
+        if not (0 <= next_state.row < self.row_length):
+            next_state = state
+        if not (0 <= next_state.column < self.column_length):
+            next_state = state
+
+        # Check whether the agent bumped a block cell.
+        if self.grid[next_state.row][next_state.column] == 9:
+            next_state = state
+
+        return next_state
+
+    def reward_func(self, state):
+        reward = self.default_reward
+        done = False
+
+        # Check an attribute of next state.
+        attribute = self.grid[state.row][state.column]
+        if attribute == 1:
+            # Get reward! and the game ends.
+            reward = 1
+            done = True
+        elif attribute == -1:
+            # Get damage! and the game ends.
+            reward = -1
+            done = True
+
+        return reward, done
+
+
